ce08204a63a07c511e378b7f1f5630bfb0766ecb
Deepak Mardi <deepakmardi22@gmail.com>
2026-03-17 16:28:48 +0530

    feat: Add PostgreSQL database connection and query utilities, and initialize the database connection on application startup.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,9 @@
 import app from "./app";
+import { connectDB } from "./config/db";
 
 const PORT = process.env.PORT || 5000;
+
+await connectDB();
 
 app.listen(PORT, () => {
   console.log(`Server is running http://localhost:${PORT}`);
