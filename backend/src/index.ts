42ae8faa8a6a7e003305a5c45a55ea04a195530d
Deepak Mardi <deepakmardi22@gmail.com>
2026-03-17 16:48:41 +0530

    feat: Implement admin user initialization and synchronization on application startup using environment variables and bcrypt.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,11 @@
 import app from "./app";
 import { connectDB } from "./config/db";
+import { initAdmin } from "./config/init";
 
 const PORT = process.env.PORT || 5000;
 
 await connectDB();
+await initAdmin();
 
 app.listen(PORT, () => {
   console.log(`Server is running http://localhost:${PORT}`);
